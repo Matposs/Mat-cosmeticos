6805a75372af189c8627a1803fae9303a32daa54
Matheus <matheusposs2012@gmail.com>
2024-07-12 15:51:18 -0300

    alteracoes dos detalhes com a url em prod

--- config/config.js
+++ config/config.js
@@ -1,3 +1,3 @@
 export const URL_TESTE = "http://localhost:3000/";
 export const URL_DESENV = "http://localhost:3000/";
-export const URL_PROD = "https://api-mat-cosmeticos.vercel.app/";
+export const URL_PROD = "https://api-mat-cosmeticos.vercel.app";
