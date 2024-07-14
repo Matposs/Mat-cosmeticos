b2617702360a1c34dcaa493625f8a86619aa3fd1
Matheus <matheusposs2012@gmail.com>
2024-07-14 15:05:11 -0300

    Alteracoes no criar conta e no css

--- scripts/script-criar-conta.js
+++ scripts/script-criar-conta.js
@@ -1,5 +1,6 @@
 const form = document.getElementById('signupForm');
 import { URL_TESTE, URL_DESENV, URL_PROD } from '../config/config.js';
+
 form.addEventListener('submit', async function (event) {
     event.preventDefault();
 
@@ -9,6 +10,19 @@ form.addEventListener('submit', async function (event) {
     const senha = formData.get('senha');
     const senhaConfirmar = formData.get('senhaConfirmar');
 
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(email)) {
+        alert("Por favor, insira um email válido.");
+        return;
+    }
+    if (senha.length < 5) {
+        alert("A senha deve ter pelo menos 5 caracteres.");
+        return;
+    }
+    if (senha !== senhaConfirmar) {
+        alert("As senhas não correspondem.");
+        return;
+    }
     try {
         const response = await fetch(`${URL_PROD}/usuarios/registro`, {
             method: 'POST',
